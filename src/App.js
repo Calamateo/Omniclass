90679effbbde9094621b77d93eba93cae60c0f33
Calamateo <dany.calamateo@gmail.com>
2022-06-21 11:45:21 -0500

    Se añadio la tabla con todos los status

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import { TableAPI } from './components/TableApi';
+import { TableProvider } from './Context/TableContext';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <TableProvider >
+      <div className="">
+      <Navbar />
+      <TableAPI />
+      </div>
+      </TableProvider>
   );
 }
 
